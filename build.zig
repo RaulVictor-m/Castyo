6ab8c910b15d278fbd58016c53ffc1aa59200185
RaulVictor-m <raulvictor822@gmail.com>
2025-08-25 21:21:25 -0300

    updating build.zig to support zig 0.15.1

--- build.zig
+++ build.zig
@@ -33,9 +33,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/castyo.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = castyo_mod,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
